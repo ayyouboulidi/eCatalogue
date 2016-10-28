e0fe677e3966f6b4711ca2174478598f7b34f85b
Benjamin Dant <benjamindant@gmail.com>
2016-10-28 17:33:52 +0200

    Added welcome root view

---
+++ src/client/root/welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from '../elements/header';
+
+class Welcome extends Component {
+    render(){
+        return(
+            <div className="welcome">
+                <Header />
+            </div>
+        );
+    }
+}
+export default Welcome;
