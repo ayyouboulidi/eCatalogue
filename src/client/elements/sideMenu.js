bb2ddfb2f50d003c45fbbaf6ed282f611a155d24
Benjamin Dant <benjamindant@gmail.com>
2016-10-28 16:35:01 +0200

    Added Side menu elements

--- src/client/elements/sideMenu.js
+++ src/client/elements/sideMenu.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default class SideMenu extends React.Component{
   render() {
     return (
-      <div>
+      <div className="side_menu">
+        <div className="side_menu_box">Dashboard</div>
+        <div className="side_menu_box">Cabin</div>
+        <div className="side_menu_box">Cabin System</div>
+        <div className="side_menu_box">System</div>
+        <div className="side_menu_box">Emergency Equipment</div>
+        <div className="side_menu_box">My Favourites</div>
+        <div className="side_menu_box">My Project</div>
       </div>
     )
   }
