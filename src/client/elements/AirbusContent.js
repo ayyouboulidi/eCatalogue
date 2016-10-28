edfe0b14613b73616f74ee83f9af2c736c296d73
Benjamin Dant <benjamindant@gmail.com>
2016-10-28 17:29:23 +0200

    Added Airbus views component

---
+++ src/client/elements/AirbusContent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Breadcrumb from './Breadcrumb';
+import Customers from './Customers';
+import Updates from './Updates';
+import MostViewed from './MostViewed';
+
+class MainContent extends Component {
+    render(){
+        return(
+            <div className="content">
+                <Breadcrumb location="Dashboard" />
+                <div className="overview">
+                    <Customers />
+                    <Updates />
+                    <MostViewed />
+                </div>
+            </div>
+        );
+    }
+}
+export default MainContent;
