fc6db036939657eae48e35ea1f1c2f573a72aec5
Benjamin Dant <benjamindant@gmail.com>
2016-10-29 22:03:36 +0200

    Added top section of the welcome view

--- src/client/root/welcome.js
+++ src/client/root/welcome.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Link, Router, Route, IndexRoute, browserHistory } from 'react-router'
 import Header from '../elements/header';
+import Breadcrumb from '../elements/Breadcrumb';
+import PageTitle from '../elements/PageTitle';
+import Search from '../elements/Search';
 
 class Welcome extends Component {
     render(){
         return(
             <div className="welcome">
                 <Header />
+                <Breadcrumb title="My Airbus" location="Aircraft" />
+                <section className="welcome">
+                    <div className="welcome-top">
+                        <PageTitle title="Welcome to My Airbus" excerpt="Please select an aircraft to continue" />
+                        <Search />
+                    </div>
+                    <div className="welcome-middle">
+                    </div>
+                </section>
             </div>
         );
     }
