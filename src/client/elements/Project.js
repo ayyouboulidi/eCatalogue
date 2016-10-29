6f042f00e18a1c1032a904482aff633889f7e099
Benjamin Dant <benjamindant@gmail.com>
2016-10-29 20:01:51 +0200

    Added component files related to Airlines Content component

---
+++ src/client/elements/Project.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+    render(){
+        return(
+            <div className="project">
+            </div>
+        );
+    }
+}
+export default Project;
