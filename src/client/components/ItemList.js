4decd68dc7654df0be9fd8d4cf84546f1680ac90
OULIDI-OMALI <ayyoub.oulidi-omali@capgemini.com>
2016-10-30 17:10:16 +0100

    list of items

--- src/client/components/ItemList.js
+++ src/client/components/ItemList.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export default class ItemList extends Component {
+    constructor(props){
+      super(props)
+      this.state={
+        items:[1,2,3,4,5,6,7,8,2,3,4,5,6,7,8,2,3,4,5,6,7,8,2,3,4,5,6,7,8,2,35,6,7,8,2,3,4,5,6,7,8]
+      }
+    }
     render(){
+      let items = this.state.items
         return(
-            <div className="width75 bgcurrentproject">
-              MonumentsList
+            <div className="width75 bgcurrentproject activeoverflow height300">
+              {items.map(function(item,key){
+                return(
+                  <img key={key} src="img/galley.png"/>
+                )
+              })}
             </div>
         );
     }
