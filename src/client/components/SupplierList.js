72e0399dd4b74d946208c06ed26217e1768a27de
OULIDI-OMALI <ayyoub.oulidi-omali@capgemini.com>
2016-10-30 15:29:19 +0100

    add supllier list

--- src/client/components/SupplierList.js
+++ src/client/components/SupplierList.js
@@ -1,40 +1,25 @@
 import React, { Component } from 'react'
 
 export default class SupplierList extends Component {
+    constructor(props){
+      super(props)
+      this.state={
+        suppliers:[1,2,3,4,5,6,7,8,9,10,22]
+      }
+    }
     render(){
+      let suppliers = this.state.suppliers
         return(
-          <div className="pflex activeoverflowx height100">
-            <div className="verticalScroll">
-              <img src="img/galley.png"/>
+            <div className="pflex activeoverflowx height100">
+              {suppliers.map(function(supplier,key){
+                return(
+                  <div className="verticalScroll" key={key}>
+                    <div><img src="img/galley.png"/></div>
+                    <div>Name {supplier}</div>
+                  </div>
+                )
+              })}
             </div>
-            <div className="verticalScroll">
-              <img src="img/galley.png"/>
-            </div>
-            <div className="verticalScroll">
-              <img src="img/galley.png"/>
-            </div>
-            <div className="verticalScroll">
-              <img src="img/galley.png"/>
-            </div>
-            <div className="verticalScroll">
-              <img src="img/galley.png"/>
-            </div>
-            <div className="verticalScroll">
-              <img src="img/galley.png"/>
-            </div>
-            <div className="verticalScroll">
-              <img src="img/galley.png"/>
-            </div>
-            <div className="verticalScroll">
-              <img src="img/galley.png"/>
-            </div>
-            <div className="verticalScroll">
-              <img src="img/galley.png"/>
-            </div>
-            <div className="verticalScroll">
-              <img src="img/galley.png"/>
-            </div>
-          </div>
         );
     }
 }
