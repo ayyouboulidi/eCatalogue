7604864a319a746b781dc7f2d65735e2efb70801
Benjamin Dant <benjamindant@gmail.com>
2016-10-31 12:22:37 +0100

    Added reference to store/welcome rendering Aircrafts descriptions and filtering with the selected family

--- src/client/welcome/AircraftDescription.js
+++ src/client/welcome/AircraftDescription.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import welcome from '../store/welcome';
 
 class AircraftDescription extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state = {aircrafts: []};
+    }
+    componentWillMount(){
+        let filter = this.props.selected;
+        var aircrafts = welcome.getAircrafts();
+        console.log("Searching for " + filter);
+        aircrafts.forEach(function(aircraft) {
+            if (aircraft.family == filter) {
+                this.state.aircrafts.push(aircraft);
+            }
+        }, this);
+        console.log(this.state);
+        this.setState(this.state);
+    }
+    handleSelect(event){
+        console.log("Selected : " + event.target);
     }
     render(){
         return(
             <div className="aircraft_description">
-                {this.props.aircrafts}
+                <div>
+                    {this.props.selected}
+                </div>
+                {
+                    this.state.aircrafts.map((aircraft, key)=>{
+                        return(
+                            <div key={key} onClick={this.handleSelect.bind(this)}>
+                                <div>{aircraft.type}</div>
+                                <div>{aircraft.description}</div>
+                            </div>
+                        );
+                    })
+                }
             </div>
         );
     }
