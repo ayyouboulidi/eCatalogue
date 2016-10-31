fad962202b6e21e7bdb45976661416d66f785592
Benjamin Dant <benjamindant@gmail.com>
2016-10-31 12:19:46 +0100

    Modified Aircraft Selection to call a single component : AircraftTypes

--- src/client/welcome/AircraftSelection.js
+++ src/client/welcome/AircraftSelection.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
 import AircraftTypes from './AircraftTypes';
-import AircraftDescription from './AircraftDescription';
-import aircrafts from '../store/welcome';
 
 class AircraftSelection extends Component {
-    constructor(){
-        super();
-        this.state = {aircrafts: []};
-    }
-    componentWillMount(){
-        this.state.aircrafts = aircrafts.getAircrafts();
-        this.setState(this.state);
-    }
     render(){
         return(
             <div className="aircraft_selection">
-                <AircraftTypes aircrafts={this.state.aircrafts} />
-                <AircraftDescription aircrafts="A380" />
+                <AircraftTypes />
             </div>
         );
     }
