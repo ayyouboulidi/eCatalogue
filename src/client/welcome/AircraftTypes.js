60bf3412ecd5a0bd4ae5cba6ba212762d0b3a6a9
Benjamin Dant <benjamindant@gmail.com>
2016-10-31 12:21:02 +0100

    Added reference to store/welcome rendering stored values

--- src/client/welcome/AircraftTypes.js
+++ src/client/welcome/AircraftTypes.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import welcome from '../store/welcome';
+import AircraftDescription from './AircraftDescription';
 
 class AircraftTypes extends Component {
+    constructor(props){
+        super(props);
+        this.state = {families: [], selected: ""};
+    }
+    componentWillMount(){
+        this.state.families = welcome.getFamily();
+        this.state.selected = "A380";
+        this.setState(this.state);
+    }
+    handleSelect(event){
+        console.log("Selected : " + event.target);
+    }
     render(){
+        var self = this;
         return(
             <div className="aircraft_types">
-                Aircraft Types
+                <div className="carousel">
+                Airbus Families
+                    {
+                        this.state.families.map(function(family, key){
+                            return(
+                                <div key={key} className="aircraft_card" onClick={self.handleSelect.bind(this)}>
+                                    <div name={family.name}>{family.name}</div>
+                                    <div>{family.description}</div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <AircraftDescription selected={this.state.selected} />
             </div>
         );
     }
