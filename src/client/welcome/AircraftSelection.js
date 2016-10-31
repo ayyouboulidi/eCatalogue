9b0ddea979c98948143bf974dd71d716ab10d735
Benjamin Dant <benjamindant@gmail.com>
2016-10-31 10:18:49 +0100

    Added reference to welcome rx.js store

--- src/client/welcome/AircraftSelection.js
+++ src/client/welcome/AircraftSelection.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import AircraftTypes from './AircraftTypes';
 import AircraftDescription from './AircraftDescription';
+import aircrafts from '../store/welcome';
 
 class AircraftSelection extends Component {
+    constructor(){
+        super();
+        this.state = {aircrafts: []};
+    }
+    componentWillMount(){
+        this.state.aircrafts = aircrafts.getAircrafts();
+        this.setState(this.state);
+    }
     render(){
         return(
             <div className="aircraft_selection">
-                <AircraftTypes aircrafts="A380" />
+                <AircraftTypes aircrafts={this.state.aircrafts} />
                 <AircraftDescription aircrafts="A380" />
             </div>
         );
