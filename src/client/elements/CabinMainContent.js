1ccf1ef02c02ee38bfab2ee87b8f91fcf2221924
OULIDI-OMALI <ayyoub.oulidi-omali@capgemini.com>
2016-11-01 10:51:43 +0100

    add switcher button and store

--- src/client/elements/CabinMainContent.js
+++ src/client/elements/CabinMainContent.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react'
-import SearchBox from "../components/searchbox"
-import Title from "../components/title"
 import MonumentsList from "../components/MonumentsList"
 import SupplierList from "../components/SupplierList"
 import FilterPanel from "../components/FilterPanel"
 import ItemList from "../components/ItemList"
+import HeadCabin from "../components/HeadCabin"
 
 export default class MainContent extends Component {
+
     render(){
         return(
             <div className="content">
-              <div className="headdashboard">
-                <Title />
-                <SearchBox />
-              </div>
+              <HeadCabin/>
               <div className="height250 bgcurrentproject">
                 <MonumentsList />
               </div>
