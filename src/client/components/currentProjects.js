92e7232a88faf7ca525248a9ba648f98d52a2272
Benjamin Dant <benjamindant@gmail.com>
2016-11-02 00:39:09 +0100

    Added class name, automation with item values and modified view details to button

--- src/client/components/currentProjects.js
+++ src/client/components/currentProjects.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
+import projectStore from "../store/projectList"
 import ActionsMenu from "./actionMenu"
 import ProjectList from "./projectList"
 
 export default class currentProjects extends Component {
     render(){
+      let projects = projectStore.getProjects();
         return(
             <div className="width50 fleft bgcurrentproject prelative">
               <div className="height50">
-                <div className="fleft">My Current projects</div>
+                <div className="second_title fleft">My Current projects</div>
                 <ActionsMenu className="fright"/>
               </div>
               <div className="activeoverflow height400 mleft mright">
                 <ProjectList />
               </div>
               <div className="width100 pabsolute pbottom">
-                <div className="fleft"><b>13</b> Items</div>
-                <div className="fright"><img src="img/VIEW-DETAILS.png"/></div>
+                <div className="fleft"><b>{projects.length}</b> Items</div>
+                <div className="fright"><input type="submit" className="view_details" value="View Details" /></div>
               </div>
             </div>
         );
