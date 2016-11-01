a72c20355e9a9aa3e95c8d4a0053d43700cddb30
OULIDI-OMALI <ayyoub.oulidi-omali@capgemini.com>
2016-11-01 11:02:47 +0100

    switcher added

---
+++ src/client/elements/AreaMode.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AreaMode extends Component {
+
+    render(){
+        return(
+            <div>
+                Area
+            </div>
+        );
+    }
+}
--- src/client/elements/CabinMainContent.js
+++ src/client/elements/CabinMainContent.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react'
-import MonumentsList from "../components/MonumentsList"
-import SupplierList from "../components/SupplierList"
-import FilterPanel from "../components/FilterPanel"
-import ItemList from "../components/ItemList"
+import AreaMode from "./AreaMode"
+import CatalogMode from "./CatalogMode"
 import HeadCabin from "../components/HeadCabin"
+import switcherView from "../store/switcherView"
 
 export default class MainContent extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      viewMode:null
+    }
+  }
+  componentWillMount(){
+    this.setState({viewMode:switcherView.getView()})
+  }
+
+  componentDidMount() {
+    this.disposable = switcherView.getStore$().subscribe((newView) => {
+      this.state.viewMode = newView;
+      this.setState(this.state);
+    })
+  }
+
+  componentWillUnmount() {
+    this.disposable.dispose()
+  }
 
     render(){
+      let switcher = this.state.viewMode
         return(
             <div className="content">
               <HeadCabin/>
-              <div className="height250 bgcurrentproject">
-                <MonumentsList />
-              </div>
-              <div className="height250 bgchecklist">
-                <SupplierList />
-              </div>
-              <div className="height300 bgcontact pflex">
-                <FilterPanel/>
-                <ItemList />
-              </div>
+              {switcher === "Catalog"?<CatalogMode />:<AreaMode />}
             </div>
         );
     }
