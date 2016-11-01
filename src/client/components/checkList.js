08621275ed08f7cc435e79273b8b59c017518b9b
Benjamin Dant <benjamindant@gmail.com>
2016-11-02 00:28:14 +0100

    Added class name

--- src/client/components/checkList.js
+++ src/client/components/checkList.js
@@ -6,7 +6,7 @@ export default class checkList extends Component {
       return(
           <div className="width25 fright bgchecklist prelative">
             <div className="height50">
-              <div className="fleft">My checklist</div>
+              <div className="second_title fleft">My checklist</div>
               <div className="fright padding2"><img src="img/EXPORT-ICON.png"/></div>
             </div>
             <div className="height400 mleft mright">
