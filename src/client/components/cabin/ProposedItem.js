6605d931a2e9cf7ff68aa7e79c6b26c2a19fc929
OULIDI-OMALI <ayyoub.oulidi-omali@capgemini.com>
2016-11-01 13:34:10 +0100

    hide/display proposed items on click action

--- src/client/components/cabin/ProposedItem.js
+++ src/client/components/cabin/ProposedItem.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react'
+import zoneFilter from '../../store/ZoneFilter'
+import AccordionFilter from '../../store/AccordionFilter'
 
 export default class ProposedItem extends Component {
   constructor(props){
     super(props)
     this.state={
-      proposedItems:[1,2,3,4,5,6,7,8,9,10,22]
+      proposedItems:[1,2,3,4,5,6,7,8,9,10,22],
+      aircraftselectedzone:false,
+      accordionselectedelement:false
     }
   }
+
+  componentWillMount(){
+    this.setState({aircraftselectedzone:zoneFilter.getAircraftZone(),accordionselectedelement:AccordionFilter.getAccordionElement()})
+  }
+
+
+  componentDidMount() {
+  this.listeners = [];
+  this.listeners.push(zoneFilter.getStore$().subscribe((newZone) =>{
+    this.state.aircraftselectedzone = newZone;
+    this.setState(this.state);
+  }));
+
+  this.listeners.push(AccordionFilter.getStore$().subscribe((newZone) => {
+    this.state.accordionselectedelement = newZone;
+    this.setState(this.state);
+  }))
+  this.setState(this.state)
+}
+
+componentWillUnmount() {
+  this.listeners.forEach(function(listener) {
+    if(typeof listener.dispose === "function") {
+      listener.dispose();
+    }
+  })
+}
+
   render(){
     let proposedItems = this.state.proposedItems
+    let displayItems = this.state.aircraftselectedzone || this.state.accordionselectedelement
       return(
           <div className="pflex activeoverflowx height100">
-            {proposedItems.map(function(proposedItem,key){
-              return(
-                <div className="verticalScroll" key={key}>
-                  <div><img src="img/galley.png"/></div>
-                  <div><b>Galley</b></div>
-                  <div>Description {proposedItem}</div>
-                </div>
-              )
-            })}
+            {
+              displayItems ?
+              proposedItems.map(function(proposedItem,key){
+                return(
+                  <div className="verticalScroll" key={key}>
+                    <div><img src="img/galley.png"/></div>
+                    <div><b>Galley</b></div>
+                    <div>Description {proposedItem}</div>
+                  </div>
+                )
+              })
+              : null
+          }
           </div>
       );
   }
