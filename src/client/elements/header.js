17cb27bfa901d7a85287fde556a9e249014819b3
Benjamin Dant <benjamindant@gmail.com>
2016-11-01 17:55:56 +0100

    Linked logo to first page WIP further link it to Welcome Page

--- src/client/elements/header.js
+++ src/client/elements/header.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router'
 import UserProfile from './userProfile'
 
-export default class Header extends React.Component{
+export default class Header extends Component{
   render() {
     return (
       <header>
-        <div className="header_left"><img src="img/AIRBUS-LOGO.png"/><span className="main_title">{" "}My <b>airbus</b> - Online Catalog</span></div>
+        <div className="header_left"><Link to="/"><img src="img/AIRBUS-LOGO.png"/></Link><span className="main_title">{" "}My <b>airbus</b> - Online Catalog</span></div>
         <UserProfile/>
       </header>
     )
