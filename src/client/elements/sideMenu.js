f901666268b08fd8a73453673788dedb9bfa2af0
Benjamin Dant <benjamindant@gmail.com>
2016-11-01 17:56:52 +0100

    Added links and design to sidebar

--- src/client/elements/sideMenu.js
+++ src/client/elements/sideMenu.js
@@ -2,16 +2,27 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default class SideMenu extends React.Component{
+  constructor(props){
+    super(props);
+    let url = window.location.href;
+    let urlSplit = url.split('/');
+    let view = urlSplit[urlSplit.length - 1];
+    this.state = { selected: view};
+  }
   render() {
+    let cabinStyle = {
+      "height": "65%",
+      "width": "100%"
+    }
     return (
       <div className="side_menu">
-        <div className="side_menu_box"><Link to="/dashboard">Dashboard</Link></div>
-        <div className="side_menu_box"><Link to="/cabin">cabin</Link></div>
-        <div className="side_menu_box">Cabin System</div>
-        <div className="side_menu_box">System</div>
-        <div className="side_menu_box">Emergency Equipment</div>
-        <div className="side_menu_box">My Favourites</div>
-        <div className="side_menu_box">My Project</div>
+        <Link to="/dashboard" ><div className={(this.state.selected == "dashboard") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/DASHBOARD-ICON.png"/><span>Dashboard</span></div></Link>
+        <Link to="/cabin"><div className={(this.state.selected == "cabin") ? "side_menu_box selected" : "side_menu_box"}><img style={cabinStyle} src="/img/CABIN-ICON.png"/><span>Cabin</span></div></Link>
+        <Link to="" ><div className={(this.state.selected == "cabin_system") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/CABIN-SYSTEM-ICON.png"/><span>Cabin System</span></div></Link>
+        <Link to="" ><div className={(this.state.selected == "system") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/SYSTEM-ICON.png"/><span>System</span></div></Link>
+        <Link to="" ><div className={(this.state.selected == "emergency") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/EMERGENCY-ICON.png"/><span>Emergency Equipment</span></div></Link>
+        <Link to="" ><div className={(this.state.selected == "favorites") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/FAVOURITE-ICON.png"/><span>My Favourites</span></div></Link>
+        <Link to="" ><div className={(this.state.selected == "project") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/MY-PROJECT-ICON.png"/><span>My Project</span></div></Link>
       </div>
     )
   }
