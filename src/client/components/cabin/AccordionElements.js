c24e952ba6be70f6f2db43f8bfc51fe1339b760a
OULIDI-OMALI <ayyoub.oulidi-omali@capgemini.com>
2016-11-01 12:41:44 +0100

    implement logic behind the zone click

--- src/client/components/cabin/AccordionElements.js
+++ src/client/components/cabin/AccordionElements.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react'
 import {Accordion,Panel} from 'react-bootstrap'
+import zoneFilter from '../../store/ZoneFilter'
+import AccordionFilter from '../../store/AccordionFilter'
 
 export default class AccordionElements extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      aircraftselectedzone:false,
+      accordionselectedelement:false
+    }
+  }
+
+  componentWillMount(){
+    this.setState({aircraftselectedzone:zoneFilter.getAircraftZone(),accordionselectedelement:AccordionFilter.getAccordionElement()})
+  }
+
+
+  componentDidMount() {
+  this.listeners = [];
+  this.listeners.push(zoneFilter.getStore$().subscribe((newZone) =>{
+    this.state.aircraftselectedzone = newZone;
+    this.setState(this.state);
+  }));
+
+  this.listeners.push(AccordionFilter.getStore$().subscribe((newZone) => {
+    this.state.accordionselectedelement = newZone;
+    this.setState(this.state);
+  }))
+  this.setState(this.state)
+}
+
+componentWillUnmount() {
+  this.listeners.forEach(function(listener) {
+    if(typeof listener.dispose === "function") {
+      listener.dispose();
+    }
+  })
+}
+
+  selectZone(){
+    this.state.accordionselectedelement = this.state.aircraftselectedzone ?
+      this.state.accordionselectedelement : !this.state.accordionselectedelement
+    this.setState(this.state)
+    AccordionFilter.setAccordionElement(this.state.accordionselectedelement)
+  }
+
     render(){
+      let elements = !this.state.aircraftselectedzone && !this.state.accordionselectedelement ?
+                "all":
+                !this.state.aircraftselectedzone && this.state.accordionselectedelement ?
+                "filtered by accordion":"filtered by zone"
         return(
             <div>
               <Accordion>
-                <Panel header="Cabin" eventKey="1">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                <Panel header="Cabin" eventKey="1" onClick={this.selectZone.bind(this)}>
+                  {elements}
                 </Panel>
                 <Panel header="Cabin System" eventKey="2">
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
--- src/client/components/cabin/Cigare.js
+++ src/client/components/cabin/Cigare.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react'
+import zoneFilter from '../../store/ZoneFilter'
+import AccordionFilter from '../../store/AccordionFilter'
 
 export default class Cigare extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      aircraftselectedzone:false,
+      accordionselectedelement:false
+    }
+  }
+
+  componentWillMount(){
+    this.setState({aircraftselectedzone:zoneFilter.getAircraftZone(),accordionselectedelement:AccordionFilter.getAccordionElement()})
+  }
+
+
+  componentDidMount() {
+  this.listeners = [];
+  this.listeners.push(zoneFilter.getStore$().subscribe((newZone) =>{
+    this.state.aircraftselectedzone = newZone;
+    this.setState(this.state);
+  }));
+
+  this.listeners.push(AccordionFilter.getStore$().subscribe((newZone) => {
+    this.state.accordionselectedelement = newZone;
+    this.setState(this.state);
+  }))
+  this.setState(this.state)
+}
+
+componentWillUnmount() {
+  this.listeners.forEach(function(listener) {
+    if(typeof listener.dispose === "function") {
+      listener.dispose();
+    }
+  })
+}
+
+
+
+  selectZone(){
+    this.state.aircraftselectedzone = this.state.accordionselectedelement ?
+      this.state.aircraftselectedzone : !this.state.aircraftselectedzone
+    this.setState(this.state)
+    zoneFilter.setAircraftZone(this.state.aircraftselectedzone)
+  }
+
     render(){
+      let img = !this.state.aircraftselectedzone && !this.state.accordionselectedelement ?
+                "all":
+                this.state.aircraftselectedzone && !this.state.accordionselectedelement ?
+                "clicked":"selected"
         return(
             <div>
-              <img src="img/cigare.png"/>
+              <img src={"img/cigare-"+img+".png"} onClick={this.selectZone.bind(this)}/>
             </div>
         );
     }
