4783847343f5205437ee3ee2f62f94bd61b6caf2
Benjamin Dant <benjamindant@gmail.com>
2016-11-03 10:54:35 +0100

    Changed default image

--- src/client/components/news.js
+++ src/client/components/news.js
@@ -10,7 +10,7 @@ export default class news extends Component {
     render(){
         return(
             <div className="width75 height100 fright bgnews dashboard-news">
-              <img className="maxwidth100 maxheight100" src="img/airbus.png"/>
+              <img className="maxwidth100 maxheight100" src="img/news-default-image.png"/>
             </div>
         );
     }
