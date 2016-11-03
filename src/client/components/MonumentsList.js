a11b7fcc026370b6c15bb7dbf7d880536804a2d7
Benjamin Dant <benjamindant@gmail.com>
2016-11-03 11:14:43 +0100

    Added class names

--- src/client/components/MonumentsList.js
+++ src/client/components/MonumentsList.js
@@ -26,13 +26,13 @@ export default class MonumentsList extends Component {
       let monuments = this.state.monuments
       let _this = this
         return(
-            <div className="pflex activeoverflowx height100">
+            <div className="pflex activeoverflowx height100 monument-list">
               {monuments.map(function(monument,key){
                 return(
                   <div className="verticalScroll" key={key} id={monument.name} onClick={_this.setSelectedCategory.bind(_this)}>
                     <div><img src="img/galley.png"/></div>
-                    <div><b>{monument.name}</b></div>
-                    <div>{monument.description} id {monument.id}</div>
+                    <div className="monument-name"><b>{monument.name}</b></div>
+                    <div className="monument-description">{monument.description} id {monument.id}</div>
                   </div>
                 )
               })}
