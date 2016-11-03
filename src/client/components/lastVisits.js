b4c2611e122975b7e1d88dc414e51cce8e2b0130
Benjamin Dant <benjamindant@gmail.com>
2016-11-03 09:57:25 +0100

    Added class names

--- src/client/components/lastVisits.js
+++ src/client/components/lastVisits.js
@@ -12,14 +12,14 @@ export default class lastVisits extends Component {
             <div className="width25 fleft bgcontact">
               <div className="second_title">Last visits</div>
               <div className="activeoverflow height250">
-                <table className="width100 padding2">
+                <table className="width100 padding2 last-visits-list">
                   <tbody>
                     {
                       this.state.items.map(function(item,key){
                         return(
-                        <tr key={key}>
-                          <td><img className="fright" src="img/MAIL-ICON.png"/></td>
-                          <td>{item} - DATE 05/45/4553</td>
+                        <tr key={key} className="last-visit-details">
+                          <td className="read-status"><img className="fright" src="img/MAIL-ICON.png"/></td>
+                          <td className="last-visit-description">{item} - DATE 05/45/4553</td>
                         </tr>
                       )
                       })
