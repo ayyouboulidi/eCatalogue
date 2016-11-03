000527e51b800c68954318ca5bba370054f8d509
Benjamin Dant <benjamindant@gmail.com>
2016-11-03 11:07:05 +0100

    Modified class names

--- src/client/elements/CatalogMode.js
+++ src/client/elements/CatalogMode.js
@@ -8,14 +8,14 @@ export default class CatalogMode extends Component {
 
     render(){
         return(
-            <div>
-              <div className="height250 bgcurrentproject monument_list">
+            <div className="catalog_mode">
+              <div className="monument_list">
                 <MonumentsList />
               </div>
-              <div className="height250 bgchecklist supplier_list">
+              <div className="supplier_list">
                 <SupplierList />
               </div>
-              <div className="height300 bgcontact pflex cabin_bottom_block">
+              <div className="cabin_bottom_block">
                 <FilterPanel/>
                 <ItemList />
               </div>
