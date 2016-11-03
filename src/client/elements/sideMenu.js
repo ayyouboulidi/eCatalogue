4e35313ce34c3771047e03987ccc5b1567c96f21
Benjamin Dant <benjamindant@gmail.com>
2016-11-03 11:52:14 +0100

    Side menu correction with new files

--- src/client/elements/sideMenu.js
+++ src/client/elements/sideMenu.js
@@ -10,14 +10,10 @@ export default class SideMenu extends React.Component{
     this.state = { selected: view};
   }
   render() {
-    let cabinStyle = {
-      "height": "65%",
-      "width": "100%"
-    }
     return (
       <div className="side_menu">
         <Link to="/dashboard" ><div className={(this.state.selected == "dashboard") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/DASHBOARD-ICON.png"/><span>Dashboard</span></div></Link>
-        <Link to="/cabin"><div className={(this.state.selected == "cabin") ? "side_menu_box selected" : "side_menu_box"}><img style={cabinStyle} src="/img/CABIN-ICON.png"/><span>Cabin</span></div></Link>
+        <Link to="/cabin"><div className={(this.state.selected == "cabin") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/CABIN-ICON.png"/><span>Cabin</span></div></Link>
         <Link to="" ><div className={(this.state.selected == "cabin_system") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/CABIN-SYSTEM-ICON.png"/><span>Cabin System</span></div></Link>
         <Link to="" ><div className={(this.state.selected == "system") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/SYSTEM-ICON.png"/><span>System</span></div></Link>
         <Link to="" ><div className={(this.state.selected == "emergency") ? "side_menu_box selected" : "side_menu_box"}><img src="/img/EMERGENCY-ICON.png"/><span>Emergency Equipment</span></div></Link>
