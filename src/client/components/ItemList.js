a46dcaf64bb367a5380c53e490d467546e6fc81e
OULIDI-OMALI <ayyoub.oulidi-omali@capgemini.com>
2016-11-03 11:35:07 +0100

    WS of get items on select

--- src/client/components/ItemList.js
+++ src/client/components/ItemList.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react'
 import PopupItem from '../elements/popup'
+import categoryStore from "../store/selectedCategoryStore"
 
 export default class ItemList extends Component {
     constructor(props){
       super(props)
       this.state={
-        items:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
+        items:[],
+        filterItems:{name:"",state:false,type:""}
       }
     }
+
+    componentWillMount(){
+      let _this = this
+      $.post('/GetItems',function(data){
+        if(data.code === 0){
+          _this.state.items=data.result
+        }
+        _this.setState(_this.state)
+      },"json")
+    }
+
+    componentDidMount() {
+      this.disposable = categoryStore.getStore$().subscribe((newCategory) => {
+        this.state.filterItems = newCategory;
+        this.setState(this.state);
+        let _this = this
+        let key = this.state.filterItems.type
+        let obj = {}
+        obj[key] = this.state.filterItems.name
+        if(this.state.filterItems.state){
+          $.post('/GetItems',obj,function(data){
+            if(data.code === 0){
+              _this.state.items=data.result
+              _this.setState(_this.state)
+            }
+          },"json")
+        }
+      })
+    }
+
     render(){
       let items = this.state.items
         return(
             <div className="width75 bgcurrentproject activeoverflow height300">
               {items.map(function(item,key){
                 return(
-                  <PopupItem id={item} key={key}>
-                    <img src="img/galley.png"/>
+                  <PopupItem id={item.id} key={key}>
+                      <div>
+                          <div><img src="img/galley.png"/></div>
+                          <div><b>{item.name}</b></div>
+                          <div>{item.title} id {item.id}</div>
+                      </div>
                   </PopupItem>
                 )
               })}
