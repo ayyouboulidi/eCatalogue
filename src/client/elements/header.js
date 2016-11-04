0e3dcf67d4622149ba8d6e4b3350eac57a03d987
Benjamin Dant <benjamindant@gmail.com>
2016-11-04 23:35:46 +0100

    Automate header

--- src/client/elements/header.js
+++ src/client/elements/header.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import UserProfile from './userProfile'
+import connectStore from '../store/connect'
 
 export default class Header extends Component{
   render() {
+    let content = "";
+    if(connectStore.getUser() != undefined){
+      content = <span className="main_title">{" "}My <b>airbus</b> - Online Catalog</span>;
+    }
     return (
       <header>
-        <div className="header_left"><Link to="/"><img src="img/AIRBUS-LOGO.png"/></Link><span className="main_title">{" "}My <b>airbus</b> - Online Catalog</span></div>
+        <div className="header_left"><Link to="/"><img src="img/AIRBUS-LOGO.png"/></Link>{content}</div>
         <UserProfile/>
       </header>
     )
--- src/client/elements/userProfile.js
+++ src/client/elements/userProfile.js
@@ -3,10 +3,15 @@ import connectStore from "../store/connect"
 
 export default class UserProfile extends React.Component{
   render() {
-    return (
-      <div className="user_profile">
-        <img src="img/USER-ICON.png"/><span className="main_title">{" "+connectStore.getUser()}</span>
-      </div>
-    )
+    console.log(connectStore.getUser());
+    if (connectStore.getUser() != undefined) {
+      return (
+        <div className="user_profile">
+          <img src="img/USER-ICON.png"/><span className="main_title">{" "+connectStore.getUser()}</span>
+        </div>
+      )
+    } else {
+      return(<div className="no-user"></div>);
+    }
   }
 }
