9fd2825cd2ae36654d7c3b44783b014372bfe012
SABIR <reda-rahal.sabir@capgemini.com>
2016-11-04 11:27:01 +0100

    Correcting /GetFilters with the new specifications and adding /GetContacts and /SearchItems

--- src/server/models/filter.js
+++ src/server/models/filter.js
@@ -1,5 +1,6 @@
-function Filters(monument,db,monuments) {
+function Filters(monument,equipment,db,monuments) {
 	this.monument = monument;
+	this.equipment = equipment;
 	this.monuments = monuments;
 	this.db = db;
 }
@@ -12,30 +13,40 @@ function Filter(name,values){
 Filters.prototype.getFilters = function(callback) {
 	if (this.filters==undefined){
 		var result = [];
-		 if (this.monuments.indexOf(this.monument)>-1){	
-			if (result!=undefined){
-				var statement = "Select DISTINCT filter.name, filter.value from filter,items,monuments "
-				+"WHERE monuments.id = items.id_monument AND filter.id_item=items.id AND monuments.name =? "
-				+"ORDER BY filter.name" ;				
-				this.db.all(statement,[this.monument]
-				, function(err, rows) {
-						if (err){
-							callback(undefined);
-						}else{
-							rows.forEach(function (row) {
-								if (result[row.name]==undefined){
-									result[row.name]=new Filter(row.name,new Array(row.value));
-									result.push(result[row.name]);
-								}else{
-									result[row.name].values.push(row.value);
-									result[result.length-1]= result[row.name];
-								}
-							});
-								this.filters = result;
-							callback(this.filters);
-						}
-				});
+		if (this.monument == undefined || this.monuments.indexOf(this.monument)>-1){	
+			var statement = "Select DISTINCT filter.name, filter.value from filter,items";
+			var whereStatement; var param;
+			if (this.monument){
+				 statement +=",monuments ";
+				 whereStatement =  "AND monuments.id = items.id_monument AND monuments.name =? ";
+				 param = new Array(this.monument);
+			}else{	
+				 statement +=",supplier ";
+				 whereStatement =  "AND supplier.id = items.id_supplier AND supplier.name =? ";
+				 param = new Array(this.equipment);
 			}
+			statement += "WHERE  filter.id_item=items.id "+whereStatement
+			+"ORDER BY filter.name" ;		
+			console.log(statement);		
+			this.db.all(statement,param
+			, function(err, rows) {
+					if (err){
+						console.log(err);
+						callback(undefined);
+					}else{
+						rows.forEach(function (row) {
+							if (result[row.name]==undefined){
+								result[row.name]=new Filter(row.name,new Array(row.value));
+								result.push(result[row.name]);
+							}else{
+								result[row.name].values.push(row.value);
+								result[result.length-1]= result[row.name];
+							}
+						});
+						this.filters = result;
+						callback(this.filters);
+					}
+				});
 		 }else{
 			result = undefined;
 			callback(result); 
