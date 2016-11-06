6118f520570d291909d1547ef55e659023d5660e
OULIDI-OMALI <ayyoub.oulidi-omali@capgemini.com>
2016-11-06 15:12:59 +0100

    config panel

---
+++ src/client/components/config/AdditionalInfoEquipment.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import PopupItem from "../../elements/popup"
+
+
+export default class AdditionalInfoEquipment extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      items:[],
+    }
+  }
+
+  componentWillMount(){
+    let _this = this
+    $.post('/GetItems',{equipment:"Gain"},function(data){
+      if(data.code === 0){
+        _this.state.items=data.result
+        _this.setState(_this.state)
+      }
+    },"json")
+  }
+    render(){
+      let items = this.state.items
+        return(
+          <div className="width80 bgcurrentproject activeoverflow height300">
+            <div className="item-list">
+              {items.map(function(item,key){
+                return(
+                  <PopupItem className="popup-item" id={item.id} key={key}>
+                      <div className="item">
+                          <div><img src={"img"+item.url_image}/></div>
+                          <div><b>{item.name}</b></div>
+                      </div>
+                    </PopupItem>
+                )
+              })}
+            </div>
+          </div>
+        );
+    }
+}
---
+++ src/client/components/config/AdditionalInfoGalley.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+
+export default class AdditionalInfoGalley extends Component {
+
+    render(){
+        return(
+            <div className="width80 bgg height300">
+              <table>
+              <tbody>
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <th colSpan="3">Master 1</th>
+                  <th colSpan="3">Master 2</th>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td>Pre-Conf 1</td>
+                  <td>PC2</td>
+                  <td>PC3</td>
+                  <td>Pre-Conf 1</td>
+                  <td>PC2</td>
+                  <td>PC3</td>
+                </tr>
+                <tr>
+                  <td>i</td>
+                  <td>Standard Unit</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                </tr>
+                <tr>
+                  <td>i</td>
+                  <td>Standard Unit</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                </tr>
+                <tr>
+                  <td>i</td>
+                  <td>Standard Unit</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                  <td>4</td>
+                </tr>
+              </tbody>
+              </table>
+            </div>
+        );
+    }
+}
