d06fce5fc4e28706ad2caa847727f389a6e2faa8
OULIDI-OMALI <ayyoub.oulidi-omali@capgemini.com>
2016-11-06 16:11:22 +0100

    add popup to matrice

--- src/client/components/config/AdditionalInfoGalley.js
+++ src/client/components/config/AdditionalInfoGalley.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
+import PopupImg from "../../elements/popupImg"
 
 
 export default class AdditionalInfoGalley extends Component {
-
+constructor(props){
+  super(props)
+  this.state={
+    tabs:[
+      {name:"Standard Unit",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"ARINC Size 1",pc11:2,pc12:null,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Beverage Maker",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Espresso/Cappuccino",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Water Heater",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Beverage Cup",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"ARINC Size 2",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Steam Oven",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Convection Oven",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Multifunctional Unit",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"3-Mode Fridge/Freezer",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"SCS-Chiller",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Warming Compartment",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Ice drawer",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"ARINC Size 4",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Microwave",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"Sink",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0},
+      {name:"GWDU",pc11:1,pc12:2,pc13:3,pc21:0,pc22:0,pc23:0}
+    ]
+  }
+}
     render(){
+      let tabs = this.state.tabs
         return(
             <div className="width80 bgg height300">
-              <table>
+              <table className="width100">
               <tbody>
                 <tr>
                   <th></th>
@@ -17,43 +43,25 @@ export default class AdditionalInfoGalley extends Component {
                 <tr>
                   <td></td>
                   <td></td>
-                  <td>Pre-Conf 1</td>
-                  <td>PC2</td>
+                  <td ><PopupImg id="pc11">Pre-Conf 1</PopupImg></td>
+                  <td  ><PopupImg id="pc12">PC2</PopupImg></td>
                   <td>PC3</td>
-                  <td>Pre-Conf 1</td>
-                  <td>PC2</td>
+                  <td ><PopupImg id="pc21">Pre-Conf 1</PopupImg></td>
+                  <td ><PopupImg id="pc22">PC2</PopupImg></td>
                   <td>PC3</td>
                 </tr>
-                <tr>
-                  <td>i</td>
-                  <td>Standard Unit</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                </tr>
-                <tr>
-                  <td>i</td>
-                  <td>Standard Unit</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                </tr>
-                <tr>
-                  <td>i</td>
-                  <td>Standard Unit</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                  <td>4</td>
-                </tr>
+                {tabs.map(function(tab,key){
+                  return(
+                  <tr key={key}>
+                    <td>i</td>
+                    <td>{tab.name}</td>
+                    <td>{tab.pc11}</td>
+                    <td>{tab.pc12}</td>
+                    <td>{tab.pc13}</td>
+                    <td>{tab.pc21}</td>
+                    <td>{tab.pc22}</td>
+                    <td>{tab.pc23}</td>
+                  </tr>)})}
               </tbody>
               </table>
             </div>
---
+++ src/client/elements/popupImg.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import popupStore from "../store/popupElement"
+import UserStore from "../store/connect"
+import switcherStore from "../store/switcherView"
+import {Modal, Button} from 'react-bootstrap'
+
+export default class popupImg extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      display:false,
+    }
+  }
+
+  close(){
+    this.state.display = false
+    this.setState(this.state);
+  }
+
+  open(e){
+    let item = e.currentTarget.id
+    this.state.display = true
+    this.setState(this.state)
+  }
+
+
+
+
+  render() {
+
+    const backdropStyle = {
+      backgroundColor: 'rgba(0, 0, 0, 0.13)',
+    }
+    return (
+      <span className='modal-example'>
+      <span id={this.props.id} onClick={this.open.bind(this)}>{this.props.children}</span>
+      <Modal id="popimg" aria-labelledby='modal-label' backdropStyle={backdropStyle} show={this.state.display} onHide={this.close.bind(this)}>
+      <div>
+      <h4 id='modal-label'></h4>
+      <img src="img/CLOSE-ICON.png" onClick={this.close.bind(this)}/>
+      <div>
+      <img src={"img/matrice/"+this.props.id+".png"}/>
+      </div>
+      </div>
+    </Modal>
+    </span>
+  )
+}
+
+}
