289baae50a3bbc96657784f17cc337165e0317fe
Benjamin Dant <benjamindant@gmail.com>
2016-11-07 22:59:29 +0100

    Fixed update project list count items

--- src/client/components/currentProjects.js
+++ src/client/components/currentProjects.js
@@ -2,8 +2,33 @@ import React, { Component } from 'react'
 import ActionsMenu from "./actionMenu"
 import ProjectList from "./projectList"
 import { Link } from 'react-router'
+import UserStore from '../store/connect'
 
 export default class currentProjects extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        count: 0
+      }
+    }
+    componentWillMount(){
+      let self = this;
+      let user = UserStore.getUser();
+      fetch('/GetProjects',{
+        method: "POST",
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify({
+          user: user
+        })
+      }).then((response)=>{
+        response.json().then((json)=>{
+          self.state.count = json.result.length;
+          self.setState(self.state);
+        });
+      })
+    }
     render(){
         return(
             <div className="width50 fleft bgcurrentproject prelative">
@@ -15,7 +40,7 @@ export default class currentProjects extends Component {
                 <ProjectList />
               </div>
               <div className="width100 pabsolute pbottom">
-                <div className="fleft"><b>9</b> Items</div>
+                <div className="fleft"><b>{this.state.count}</b> Items</div>
                 <div className="fright"><Link to="/project"><input type="submit" className="view_details" value="View Details" /></Link></div>
               </div>
             </div>
