c2daf734c3a9f6a0a8358739a5c0bd6b03034026
Benjamin Dant <benjamindant@gmail.com>
2016-11-07 17:04:33 +0100

    Added popup infos and selection highlight

--- src/client/components/MonumentsList.js
+++ src/client/components/MonumentsList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import categoryStroe from "../store/selectedCategoryStore"
+import PopupItem from '../elements/popup'
 
 export default class MonumentsList extends Component {
     constructor(props){
       super(props)
       this.state={
-        monuments:[]
+        monuments:[],
+        selected: ""
       }
     }
 
@@ -19,19 +21,30 @@ export default class MonumentsList extends Component {
 
     setSelectedCategory(e){
       let name = e.currentTarget.id
+      let id = e.currentTarget.getAttribute("name")
+      this.state.selected = id;
+      this.setState(this.state);
       categoryStroe.setSelectedCategory({name:name,state:true,type:"monument"})
     }
 
+    displayInfos(event) {
+      let id = event.currentTarget.getAttribute("name");
+      let currentMonument = this.state.monuments[id -1];
+    }
+
     render(){
-      let monuments = this.state.monuments
-      let _this = this
+      let monuments = this.state.monuments;
+      let _this = this;
         return(
             <div className="pflex activeoverflowx height100 monument-list">
               {monuments.map(function(monument,key){
                 return(
-                  <div className="verticalScroll" key={key} id={monument.name} onClick={_this.setSelectedCategory.bind(_this)}>
+                  <div className={(_this.state.selected == monument.id)?"verticalScroll selected":"verticalScroll"} key={key} name={monument.id} id={monument.name} onClick={_this.setSelectedCategory.bind(_this)}>
                     <div><img src={"img"+monument.url}/></div>
                     <div className="monument-name"><b>{monument.name}</b></div>
+                    <PopupItem id={monument.id} type="monument" key={key}>
+                      <div className="infos" name={monument.id} onClick={_this.displayInfos.bind(_this)}><img src="/img/INFOS-ICON.png" /></div>
+                    </PopupItem>
                   </div>
                 )
               })}
