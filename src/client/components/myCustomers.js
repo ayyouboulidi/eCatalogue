343e44d6677a8fc906a25887bfd3aa613708053c
OULIDI-OMALI <ayyoub.oulidi-omali@capgemini.com>
2016-11-09 18:22:11 +0100

    bug

--- src/client/components/myCustomers.js
+++ src/client/components/myCustomers.js
@@ -4,7 +4,7 @@ export default class myCustomers extends Component {
     render(){
         return(
             <div className=" center width50 fleft bgcurrentproject prelative">
-              <img className="height100 width100" src="img/admin.png"/>
+              <img className="height100 width100" src="img/admin.PNG"/>
             </div>
         );
     }
